ec626ac0805a968baec66518f5d744f2b5acc6d8
Steve Grunwell <sgrunwell@liquidweb.com>
2022-01-28 14:51:05 -0500

    Add empty classes for the Container and its test, along with all sorts of code quality scaffolding

---
+++ src/Container.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The dependency injection container.
+ *
+ * @package StellarWP\Container
+ */
+
+namespace StellarWP\Container;
+
+class Container {
+
+}
---
+++ tests/Unit/ContainerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Yoast\PHPUnitPolyfills\TestCases\TestCase;
+
+/**
+ * @covers StellarWP\Container\Container
+ */
+class ContainerTest extends TestCase {
+
+	/**
+	 * @test
+	 */
+	public function true_should_be_true() {
+		$this->assertTrue(true);
+	}
+}
